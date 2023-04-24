076336950e5a9f17c6ae25f44c678f0063152a19
Nadezhda Hristova <epohster@gmail.com>
2023-04-25 00:45:57 +0300

    💛 Empty favorites view to not show on init load;

--- src/components/favorites/FavoritesView.tsx
+++ src/components/favorites/FavoritesView.tsx
@@ -7,25 +7,15 @@ import { PhotoCard } from '../shared/PhotoCard';
 import { FavoritesEmpty } from './FavoritesEmpty';
 
 export function FavoritesView() {
-  const selectFavorites = (state: AppState) => state.favorites;
+  const selectFavorites = (state: AppState) => state.photos.filter(el => state.favorites.includes(el.id));
   const favorites = useSelector(selectFavorites);
-  const [favs, setFavs] = useState([] as Photo[])
-
-  // TODO rework, do not call get photos
-  useEffect(() => {
-    getPhotos()
-      .then(data => {
-        const favs = data.filter(el => favorites.includes(el.id));
-        setFavs(favs);
-      })
-  }, [favorites]);
-
+  
   return (
     <>
       <h2>Favorites</h2>
       <div className="photos-list">
-        {favs.length
-          ? favs.map(photo => <PhotoCard key={photo.id} photo={photo} />)
+        {favorites.length
+          ? favorites.map(photo => <PhotoCard key={photo.id} photo={photo} />)
           : <FavoritesEmpty />
         }
       </div>
